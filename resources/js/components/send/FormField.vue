6edf6b8a63840959cba8e11438c0da9144365954
Jason Brook <jasonlbrook@gmail.com>
2022-05-04 19:32:26 +0100

    Trying to get nova 4 assets installed to compile

--- resources/js/components/send/FormField.vue
+++ resources/js/components/send/FormField.vue
@@ -3,29 +3,59 @@
     <div class="card mb-6 overflow-hidden">
       <div class="flex w-2/3 justify-between px-8 pt-4">
         <div
-          class="help-text mr-4"
+            class="help-text mr-4"
         >Adjust an existing mail template or craft an email from scratch! You can use normal blade syntax and include attributes from this resources model&hellip;</div>
       </div>
 
       <div class="flex border-b border-40 remove-bottom-border px-8 py-4">
         <div class="w-full pb-2">
           <select
-            name="mail_template_select"
-            id="mail-template-select"
-            dusk="mail-template-select"
-            v-model="selectedTemplate"
-            class="form-control form-select mb-4"
-            v-if="hasTemplates"
+              name="mail_template_select"
+              id="mail-template-select"
+              dusk="mail-template-select"
+              v-model="selectedTemplate"
+              class="form-control form-select mb-4"
+              v-if="hasTemplates"
           >
             <option value disabled="disabled">Select Mail Template&hellip;</option>
 
             <option
-              :value="template"
-              v-for="(template, index) in templates"
-              :key="index"
-              v-text="template.name"
+                :value="template"
+                v-for="(template, index) in templates"
+                :key="index"
+                v-text="template.name"
             ></option>
           </select>
+
+          <input
+              name="subject"
+              id="subject"
+              dusk="subject"
+              type="text"
+              v-model="subject"
+              placeholder="Subject"
+              class="w-full form-control form-input form-input-bordered"
+          />
+
+          <input
+              name="send-delay-in-minutes"
+              id="send-delay-in-minutes"
+              dusk="send-delay-in-minutes"
+              type="number"
+              v-model="delayInMinutes"
+              placeholder="Send Delay (in minutes)"
+              class="w-full form-control form-input form-input-bordered mt-4"
+          />
+
+          <textarea
+              name="template_override"
+              id="template-override"
+              dusk="template-override"
+              rows="10"
+              v-model="body"
+              placeholder="Body"
+              class="w-full form-control form-input form-input-bordered py-3 h-auto mt-4"
+          ></textarea>
         </div>
       </div>
     </div>
@@ -45,8 +75,7 @@ export default {
       selectedTemplate: '',
       body: '',
       subject: '',
-      delayInMinutes: null,
-      test: ''
+      delayInMinutes: null
     }
   },
 
